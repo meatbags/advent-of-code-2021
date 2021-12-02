c6459e17971b39c5a234cf54c8b896090e7b6d91
meatbags <jxburrow@gmail.com>
2021-12-02 18:21:26 +1100

    d2

--- solutions/day1.js
+++
@@ -1,8 +0,0 @@
-// day 1
-fetch('data/1.txt')
-.then(s => s.text())
-.then(s => {
-  s = s.trim().split('\r\n').map(x => parseInt(x));
-  console.log(s.map((v, i) => s[i-1] < v).reduce((a, b) => a + b));
-  console.log(s.map((v, i) => s[i+3] > v).reduce((a, b) => a + b));
-});
