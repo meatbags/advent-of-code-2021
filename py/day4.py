d5c66a3a688c6cd2005d5c745862be3a799f0c69
meatbags <jxburrow@gmail.com>
2021-12-05 12:57:11 +1100

    update

--- py/day4.py
+++ py/day4.py
@@ -1,25 +1,19 @@
-from math import sqrt
 
 data = open('./data/4.txt').read().split('\n\n')
 numbers = [int(x) for x in data[0].split(',')]
-marked = {}
 cards = [[int(n) for n in chunk.split()] for chunk in data[1:]]
-s = int(sqrt(len(cards[0])))
-
-def check(card):
-    for n in range(s):
-        row = sum([1 for x in card[n*s:n*s+s] if x in marked])
-        col = sum([1 for x in card[n::s] if x in marked])
-        if row == s or col == s:
-            return True
-
+size = int(len(cards[0])**0.5)
+mark = {}
 silver, gold = 0, 0
+
 for n in numbers:
-    marked[n] = True
+    mark[n] = 1
     for i in range(len(cards)-1, -1, -1):
-        if check(cards[i]):
-            score = sum([x for x in cards.pop(i) if not x in marked]) * n
-            silver = silver or score
-            gold = score
+        if any((
+            sum([1 for x in cards[i][n*size:(n+1)*size] if x in mark]) == size or
+            sum([1 for x in cards[i][n::size] if x in mark]) == size
+        ) for n in range(size)):
+            gold = sum([x for x in cards.pop(i) if not x in mark]) * n
+            silver = silver or gold
 
 print(silver, gold)
